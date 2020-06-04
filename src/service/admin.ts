df066f2c3c35041fb1485a7c5e0613754d242c83
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-06-04 07:22:15 +0000

    ### Removed - 管理者サービスを削除

--- src/service/admin.ts
+++ src/service/admin.ts
@@ -10,39 +10,10 @@ export interface IAdmin {
     telephone: string;
 }
 
-export interface IGroup {
-    name: string;
-    description: string;
-}
-
 /**
  * 管理者サービス
  */
 export class AdminService extends Service {
-    /**
-     * ログイン中管理者のプロフィールを取得する
-     */
-    public async getProfile(): Promise<IAdmin> {
-        return this.fetch({
-            uri: '/admins/me',
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        })
-            .then(async (response) => response.json());
-    }
-
-    /**
-     * ログイン中管理者のグループを取得する
-     */
-    public async getGroups(): Promise<IGroup[]> {
-        return this.fetch({
-            uri: '/admins/me/groups',
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        })
-            .then(async (response) => response.json());
-    }
-
     /**
      * 管理者検索
      */
